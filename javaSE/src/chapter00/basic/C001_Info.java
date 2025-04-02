1a64fef72b06c1fae5e51fc21b939b9b5ab674e3
myungjinki <hello@mjki.dev>
2025-04-02 09:35:32 +0900

    day2: hello java

--- javaSE/src/chapter00/basic/C001_Info.java
+++ javaSE/src/chapter00/basic/C001_Info.java
@@ -1,7 +1,37 @@
 package chapter00.basic;
 
 public class C001_Info {
+	// 한 줄 주석 - 한 줄짜리 간단한 설명할 때 사용
+	
+	/*
+	 * 여러 줄 주석 - 여러 줄에 걸쳐 설명할 때 사용
+	 */
+	
+	/**
+	 * 여러 줄 주석2 - 클래스, 메서드, 변수 등의 문서를 자동생성하기 위한 주석
+	 * @param id 사용자 아이디
+	 * @return user 객체 반환
+	 */
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		// cntl + spaceBar
+		// cntl + s
+		// cntl + F11
+		System.out.println("Hello");
 	}
+	
+	
+	/**
+	 * Cntl + s				: 저장
+	 * Cntl + f11			: 프로그램 실행
+	 * Ctrl + Space			: 입력 중에 완성 기능 호출(템플릿)
+	 * Strl + D				: 한 줄 삭제
+	 * Ctrl + Shift + F		: 소스 정렬
+	 * Alt + 방향키			: 위 줄과 바꾸기
+	 * Ctrl + Alt + Up		: 한 줄 복사
+	 * Ctrl + Shift + O		: 자동 임폴트, 안쓰는 임폴트 삭제
+	 * Strl + S				: 소스코드 저장
+	 * Ctrl + Shift + S
+	 * Ctrl + /
+	 * Ctrl + shift + /
+	 */
 }
