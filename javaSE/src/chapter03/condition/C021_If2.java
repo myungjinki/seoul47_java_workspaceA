cb564d8963b3da2b66538769060416218107e411
myungjinki <hello@mjki.dev>
2025-04-04 11:50:02 +0900

    class: if one linec

---
+++ javaSE/src/chapter03/condition/C021_If2.java
@@ -0,0 +1,21 @@
+package chapter03.condition;
+
+public class C021_If2 {
+	public static void main(String[] args) {
+		// if block
+		
+		int age = 12;
+				
+		if (age >= 19) {
+			System.out.println("성인입니다.");
+			System.out.println("술집 입장.");
+		}
+		
+		System.out.println("=========");
+		
+		// if문은 { }가 없어도 명령어 1줄은 인정함
+		if (age >= 19)
+			System.out.println("성인입니다.");
+			System.out.println("술집 입장.");
+	}
+}
