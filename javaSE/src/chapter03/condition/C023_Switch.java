e9f68ec79f66287e424d3f67297a7fcf2a101a56
myungjinki <hello@mjki.dev>
2025-04-04 12:14:21 +0900

    class: switch

---
+++ javaSE/src/chapter03/condition/C023_Switch.java
@@ -0,0 +1,24 @@
+package chapter03.condition;
+
+public class C023_Switch {
+	public static void main(String[] args) {
+		// switch-case
+		int ranking = 2;
+		
+		switch (ranking) {
+		case 1:
+			System.out.println("금메달");
+			break;
+		case 2:
+			System.out.println("은메달");
+			break;
+		case 3:
+			System.out.println("동메달");
+			break;
+
+		default:
+			System.out.println("참가상");
+			break;
+		}
+	}
+}
